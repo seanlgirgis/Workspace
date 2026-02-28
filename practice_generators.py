f8be8d32df11bed4944db576f9e25786c294f543
shareuser <seanlgirgis@gmail.com>
2026-02-28 00:07:39 -0600

    Quick Update 2026-02-28 00:07

---
+++ practice_generators.py
@@ -0,0 +1,7 @@
+# Python Generators: Practice Workbench
+# ====================================
+
+print("--- Python Generators Starter ---")
+print("Run this file using: python D:\Workspace\practice_generators.py\n")
+
+# Your practice code will go here!
